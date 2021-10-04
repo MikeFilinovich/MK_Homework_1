24c26c5b71e207d5acb91bb3f786b8fb37a78a2b
MikhailFilinovich <filinovich93@mail.ru>
2021-10-04 18:07:29 +0300

    MK_Homework1

---
+++ Homework_2.py
@@ -0,0 +1,7 @@
+name = "Василий"
+surname = "Иванов"
+age = 33
+height = 182
+birth_year = 2021 - age
+bin = (name + " " +  surname + " " + str(birth_year) + " " + "ростом" + " " + str(height) + "см")
+print(bin)
